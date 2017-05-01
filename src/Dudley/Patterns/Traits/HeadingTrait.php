6da99200e2662a192ad238e80e1986f70a818e48
Jeremy Ward <jeremy.ward@webdevstudios.com>
2017-05-01 11:59:39 -0500

    Refactor filter in heading trait to be module-specific.

--- src/Dudley/Patterns/Traits/HeadingTrait.php
+++ src/Dudley/Patterns/Traits/HeadingTrait.php
@@ -4,31 +4,40 @@ namespace Dudley\Patterns\Traits;
 
 /**
  * Class HeadingTrait
+ *
  * @package Dudley\Patterns\Traits
  */
-trait HeadingTrait
-{
+trait HeadingTrait {
 	/**
-	 * @var
+	 * The heading.
+	 *
+	 * @var string $heading
 	 */
 	protected $heading;
 
 	/**
-	 * @var string
-	 */
-	protected $heading_filter = 'dudley_acf_heading';
-
-	/**
-	 * @return bool|mixed|null|void
+	 * Getter method for the heading.
+	 *
+	 * @return string
 	 */
 	public function get_heading() {
 		return $this->heading;
 	}
 
 	/**
-	 *
+	 * Print the heading to the template.
 	 */
 	public function heading() {
-		echo esc_html( apply_filters( $this->heading_filter, $this->heading ) );
+		$meta_type   = self::$meta_type;
+		$action_name = self::$action_name;
+
+		/**
+		 * Dynamic filter for the output of a module's heading.
+		 *
+		 * Filter uses the same setup as a module's action registration.
+		 *
+		 * Example: dudley_acf_banner_heading.
+		 */
+		echo esc_html( apply_filters( "dudley_{$meta_type}_{$action_name}_heading", $this->heading ) );
 	}
 }
