5342af138098485aef8901d4a50cd0931ed323a7
Jeremy Ward <jeremy.ward@webdevstudios.com>
2017-06-05 22:19:20 -0500

    Update version to 1.1.0.

--- dudley.php
+++ dudley.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://jmichaelward.com
  * Author: Jeremy Ward
  * Author URI: http://jmichaelward.com
- * Version: 1.0.0
+ * Version: 1.1.0
  * textdomain: dudley
  *
  * @package Dudley\Patterns
