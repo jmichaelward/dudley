c8c79d955e2c2750b263cbf7cf04e1fe9a82c3f1
Jeremy Ward <jeremy.ward@webdevstudios.com>
2017-09-29 14:39:23 -0500

    Add PHP doc blocks to DudleyTest class.

--- tests/src/Dudley/DudleyTest.php
+++ tests/src/Dudley/DudleyTest.php
@@ -1,16 +1,17 @@
 <?php
-
 /**
  * Class DudleyTest
  */
 class DudleyTest extends \WP_Mock\Tools\TestCase {
 	/**
+	 * Instantiated object of the Dudley class.
+	 *
 	 * @var \Dudley\Dudley
 	 */
 	public $dudley;
 
 	/**
-	 *
+	 * Setup test dependencies.
 	 */
 	public function setUp() {
 		\WP_Mock::setUp();
@@ -19,12 +20,15 @@ class DudleyTest extends \WP_Mock\Tools\TestCase {
 	}
 
 	/**
-	 *
+	 * Test teardown.
 	 */
 	public function tearDown() {
 		\WP_Mock::tearDown();
 	}
 
+	/**
+	 * Test that the plugin class instance is of type Dudley\Dudley.
+	 */
 	public function test_instanceof_dudley_plugin_class() {
 		$this->assertInstanceOf( \Dudley\Dudley::class, $this->dudley );
 	}
