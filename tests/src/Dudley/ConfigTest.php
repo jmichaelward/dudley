1bf91bce1226e28f1f73bce092cb717220120ceb
Jeremy Ward <jeremy.ward@webdevstudios.com>
2017-09-29 13:35:40 -0500

    Add tests for the Config class.

--- tests/src/Dudley/ConfigTest.php
+++ tests/src/Dudley/ConfigTest.php
@@ -1,20 +1,65 @@
 <?php
-
 /**
  * Class ConfigTest
  */
 class ConfigTest extends \WP_Mock\Tools\TestCase {
 	/**
+	 * Instance of the Config class.
 	 *
+	 * @var \Dudley\Config
+	 */
+	public $config;
+
+	/**
+	 * Instance of the Dudley plugin class.
+	 *
+	 * @var \Dudley\Dudley
+	 */
+	public $dudley;
+
+	/**
+	 * Set up test dependencies.
 	 */
 	public function setUp() {
 		\WP_Mock::setUp();
+
+		$this->dudley = new Dudley\Dudley();
+		$this->config = new \Dudley\Config( $this->dudley );
 	}
 
 	/**
-	 *
+	 * Tear down mocks.
 	 */
 	public function tearDown() {
 		\WP_Mock::tearDown();
+	}
+
+	/**
+	 * Make sure we're using an instance of the Dudley Config class.
+	 */
+	public function test_instance_of_dudley_config() {
+		$this->assertInstanceOf( \Dudley\Config::class, $this->config );
+	}
+
+	/**
+	 * Test that we get a Dudley ACF object back if the DUDLEY_META_TYPE is set to 'acf'.
+	 */
+	public function test_default_meta_type_is_acf() {
+		$reflection = new ReflectionMethod( \Dudley\Config::class, 'get_meta_type' );
+		$reflection->setAccessible( true );
+
+		$this->assertInstanceOf( \Dudley\MetaType\ACF::class, $reflection->invoke( $this->config ) );
+	}
+
+	/**
+	 * Test that get_meta_type returns false if an unsupported meta type is assigned to DUDLEY_META_TYPE.
+	 */
+	public function test_nonsupported_meta_type_returns_false() {
+		putenv( 'DUDLEY_META_TYPE', 'unregisteredmetatype' ); // @codingStandardsIgnoreLine
+
+		$reflection = new ReflectionMethod( \Dudley\Config::class, 'get_meta_type' );
+		$reflection->setAccessible( true );
+
+		$this->assertFalse( $reflection->invoke( $this->config ) );
 	}
 }
