43ee93cdf41f09f97bd33422dde80d0c52e9434e
Jeremy Ward <jeremy.ward@webdevstudios.com>
2017-09-29 10:12:23 -0500

    Add testing suite bootstrap file to project.

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Bootstrap the unit testing suite.
+ */
+
+// Load Composer autoloader.
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Set up WP_Mock.
+\WP_Mock::bootstrap();
